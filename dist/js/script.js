6cd605ab8d8d7fd47e254b622e2d928132061911
sobbey99 <sobbey99@gmail.com>
2021-05-20 09:47:41 +0300

    Modal Windows And Validated Forms

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+
+    // SLICK SLIDDER SETTINGS 
     $('.slider-section__wrapper').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_slide.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_slide.svg"></button>'
     });
 
+
+    // TABS DESCRIPTION
     $('.catalog__tabswrapper').on('click', '.catalog__tab:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -11,7 +15,7 @@ $(document).ready(function(){
       });
 
 
-
+    // DESCRIPTION OF FUNCTION FOR BACKCONTENT OF ITEMS
     function itemLink(itemClass) {
         $(itemClass).each( function(i) {
             $(this).on('click', function(e) {
@@ -21,7 +25,54 @@ $(document).ready(function(){
             })
         });
     }
-
+    // CALLING THIS FUNCTION
     itemLink('.catalog-item__link');
     itemLink('.catalog-item__backlink');
+
+
+    // MODAL WINDOW 
+    $('[data-modal="consultation"]').on('click', function(e) {
+        e.preventDefault();
+        $('#modal-consultation').fadeIn();
+    });
+    $('.modal-consultation__close').on('click', function() {
+        $('#modal-consultation, #modal-buy, #modal-thankyou').fadeOut();
+    });
+    $('.button_buy').on('click', function(e) {
+        e.preventDefault();
+        $('#modal-buy').fadeIn('fast');
+    });
+
+    $('.button_buy').each( function(i) {
+        $(this).on('click', function() {
+            $('#modal-buy .modal-consultation__subtitle').text($('.catalog-item__title').eq(i).text())
+        })
+    });
+
+
+    // VALIDATION 
+    function validateThis(selectorForm) {
+        $(selectorForm).validate( {
+            rules: {
+                username: "required",
+                usernumber: "required",
+                useremail: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                username: "Пожалуйста введите ваше имя!",
+                usernumber: "Хммм, Чёт я не вижу номера",
+                useremail: {
+                    required: 'Давай вводи эту почту ну',
+                    email: "Нормальную почту пожалуйста!"
+                }
+            }
+        })
+    };
+
+    validateThis('#modal-consultation .modal-consultation form.form');
+    validateThis('#modal-buy .modal-consultation form.form');
+    validateThis('.consultation form.form');
   });
