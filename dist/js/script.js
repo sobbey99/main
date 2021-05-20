13f5b97ab165d00dfb8af65d1272e16f70582adb
sobbey99 <sobbey99@gmail.com>
2021-05-20 11:18:33 +0300

    Masked Input and Animate CSS

--- dist/js/script.js
+++ dist/js/script.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
     });
 
 
-    // VALIDATION 
+    // VALIDATION  FUNCTION
     function validateThis(selectorForm) {
         $(selectorForm).validate( {
             rules: {
@@ -71,8 +71,52 @@ $(document).ready(function(){
             }
         })
     };
+    
 
+    //CALLING THIS FUNCTION
     validateThis('#modal-consultation .modal-consultation form.form');
     validateThis('#modal-buy .modal-consultation form.form');
     validateThis('.consultation form.form');
+
+
+    // MASK THE INPUT NUMBER
+    $('[name="usernumber"]').mask("+7 (999) 999-9999");
+
+    // SCRIPT FOR SENDING FORM ( AJAX )
+
+    $('form.form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#modal-consultation, #modal-buy').fadeOut();
+            $('#modal-thankyou').fadeIn('slow');
+
+            $('form.form').trigger("reset");
+        })
+        return false
+    });
+
+    // SMOOTH SCROLLING
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
+
+    // ARROW APEARS WHEN SCROLL DOWN 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >1100 ) {
+            $('.arrow-up').fadeIn('slow');
+        } else {
+            $('.arrow-up').fadeOut('fast');
+        }
+    })
+
+    // ACTIVATE WOW MODULE
+    new WOW().init();
   });
